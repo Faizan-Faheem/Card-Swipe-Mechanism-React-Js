d3ad6674ce82e4f0d31f835365fd7f8f4c30d353
Faizan-Faheem <faizanfaheem100@gmail.com>
2023-07-25 03:43:16 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header';
+import SimpleSlider from './components/SimpleSlider';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SimpleSlider />
+      
+    </div>
+  );
+}
+
+export default App;
